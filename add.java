cbd0591069b51fe23048577441c8fbf05c442fed
Abhi52425 <42265941+Abhi52425@users.noreply.github.com>
2018-08-23 12:48:19 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-class add
+class addition
 {
     public static void main(String[] args)
     {
-        Scanner s=new Scanner(System.in);
-        int a=s.nextInt();
-        int b=s.nextInt();
+        Scanner s1=new Scanner(System.in);
+        int a=s1.nextInt();
+        int b=s1.nextInt();
         int c=(a+b);
-        System.out.println("SUM: "+c);
+        System.out.println("SUM of numbers is: "+c);
     }
 }
